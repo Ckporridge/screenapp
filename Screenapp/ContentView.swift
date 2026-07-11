961c362fc56ea3db23ac4677be03c771dc3af689
yuniii-415 <seng_yun_hee@students.edu.sg>
2026-07-11 10:19:19 +0800

    removed space

--- Screenapp/ContentView.swift
+++ Screenapp/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
         ZStack {
             Color.blue
             VStack{
-                Text("Cheng Kun")
+                Text("ChengKun")
                     .foregroundStyle(.white)
                     .font(.largeTitle)
                     .bold()
